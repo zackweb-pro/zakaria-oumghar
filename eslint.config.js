c7eec3de698c3ae346a6c355945aa64c1d317126
zakaria oumghar <zakariaoumghar1@gmail.com>
2025-06-27 17:03:46 +0100

    modifying eslint config

--- eslint.config.js
+++ eslint.config.js
@@ -1,11 +1,53 @@
-/** @type {import('eslint').Linter.Config} */
-const config = {
-  extends: ['next/core-web-vitals'],
-  rules: {
-    'react/jsx-no-target-blank': 'off',
-    'react/prop-types': 'off',
-    'no-unused-vars': 'warn',
+import js from '@eslint/js';
+import reactPlugin from 'eslint-plugin-react';
+import reactHooksPlugin from 'eslint-plugin-react-hooks';
+import globals from 'globals';
+
+/** @type {import('eslint').Linter.Config[]} */
+const config = [
+  js.configs.recommended,
+  {
+    files: ['**/*.{js,jsx,ts,tsx}'],
+    languageOptions: {
+      ecmaVersion: 'latest',
+      sourceType: 'module',
+      globals: {
+        ...globals.browser,
+        ...globals.node,
+        ...globals.es2021,
+      },
+      parserOptions: {
+        ecmaFeatures: {
+          jsx: true,
+        },
+      },
+    },
+    plugins: {
+      react: reactPlugin,
+      'react-hooks': reactHooksPlugin,
+    },
+    rules: {
+      ...reactPlugin.configs.recommended.rules,
+      ...reactHooksPlugin.configs.recommended.rules,
+      'react/jsx-no-target-blank': 'off',
+      'react/prop-types': 'off',
+      'react/react-in-jsx-scope': 'off',
+      'no-unused-vars': 'warn',
+    },
+    settings: {
+      react: {
+        version: 'detect',
+      },
+    },
   },
-};
+  {
+    ignores: [
+      '.next/**',
+      'node_modules/**',
+      'out/**',
+      '**/*.min.js',
+    ],
+  },
+];
 
 export default config;
